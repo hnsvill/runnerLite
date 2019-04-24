62d02f72ebb1e437b0c6258792f98bcaf8376dcd
Hannah Scovill <hannah.scovill@kroger.com>
2019-04-23 19:22:37 -0700

    replaced old cli, updated presentation and readme

--- readme.md
+++ readme.md
@@ -5,12 +5,21 @@ This api has been written to use Flask with DynamoDB and the intention to deploy
 Clone this repo. In the project's directory, run the following commands:
 - `pip3 install boto3`
 - `pip3 install Flask`
+
+#### To run using the API:
+Run the following command:
 - `python3 app.py`
 
 The server will spin up on port 5000. The index has links to the endpoints for each calculation for
 each user and important links.
 
-Access these calculations from the command line
+#### To run from the command line:
+Run the following commands:
+- `python3 runnerLiteCLI.py --help` lists all of the options for the kpi you would like to calculate
+- `python3 runnerLiteCLI.py <kpiName> <userID> <optional: --datasource>`
+
+*A note on the datasource: The DynamoDB API key has not been included in this project's repo.
+The datasource defaults to a local json file included in this repo.
 
 
 ### More Information:
--- runnerLiteCLI.py
+++ runnerLiteCLI.py
@@ -1,36 +1,34 @@
-from dataAccess import *
+import argparse
 from workoutCalculations import *
-
-# TODO: Refactor to use Argparse. Add local data as an option.
-#   The finished command should look like:
-#       `runnerLite --pr5ks --40d7ae29e393582abdbcb8c726249e22`
+from dataAccess import *
 
 
-def runCalcs():
-    desiredCalc = input('Which calculation would you like to perform? Type your selection and press' +
-                        'enter.\n3-day, 1km streaks\nran more than 10km\n5k PRs\n\n').lower()
-    userID = input('What user would you like the calculation for? Type the user ID and press enter.\n\n')
-    data = getUserWorkouts(userID, 'run', 'local')
+def cliInterface():
+    parser = argparse.ArgumentParser()
 
-    calcOptions = ['ran more than 10km', '3-day, 1km streaks', '5k prs']
+    parser.add_argument('kpiName',
+                        help='Please specify which metric you would like to evaluate. The availible calculations are: \n' +
+                             'greaterThanNkmStreaks\nranMoreThanNkm\nprNks')
+    parser.add_argument('userID', help='Please specify the user. The availible IDs are:\n' +
+                                       '6bd5f3c04e6b5279aca633c2a245dd9c\n' + '4e7aaa167b9b5ff7b9b3a22dee8c2085\n' +
+                                       'c7e962db02da55209f02fe3d8a86c99d\nd77908482ed2505ebbf17ef72be2f080\n' +
+                                       '72eff89c74cc57178e02f103187ad579\n40d7ae29e393582abdbcb8c726249e22')
+    parser.add_argument('--dataSource', help='specify local or remote datasource. Default is local.', default='local',
+                        required=False)
 
-    if desiredCalc not in calcOptions:
-        print('Calculation choice is not one of the listed options. Please try again.')
-    elif data is None:
-        print('Error loading user data.')
-    else:
-        if desiredCalc == 'ran more than 10km':
-            kpi = ranMoreThanNkm(data, 10)
-            header = 'ran_more_than_10km'
-        elif desiredCalc == '3-day, 1km streaks':
-            kpi = greaterThanNkmStreaks(data, 1, 3)
-            header = 'greater_than_3km_streaks'
-        elif desiredCalc == '5k prs':
-            kpi = prNks(data, 5, 2018)
-            header = 'pace_5k_pr'
+    args = parser.parse_args()
 
-        print('{user_id: ' + str(userID) + ', ' + header + ': ' + str(kpi) + '}')
+    switcher = {
+        'greaterThanNkmStreaks': '{user_id: ' + args.userID + ', greater_than_3km_streaks: ' + str(
+            greaterThanNkmStreaks(getUserWorkouts(args.userID, 'run', args.dataSource), 1, 3)) + '}',
+        'ranMoreThanNkm': '{user_id: ' + args.userID + ', ran_more_than_10km: ' + str(
+            ranMoreThanNkm(getUserWorkouts(args.userID, 'run', args.dataSource), 10)) + '}',
+        'prNks': '{user_id: ' + args.userID + ', pace_5k_pr: ' + str(
+            prNks(getUserWorkouts(args.userID, 'run', args.dataSource), 5, 2018)) + '}'
+    }
+
+    print(switcher[args.kpiName])
 
 
 if __name__ == '__main__':
-    runCalcs()
+    cliInterface()
